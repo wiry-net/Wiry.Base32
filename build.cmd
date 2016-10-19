9991ee0f8ae447e806a3aaa90fdc62faa4919592
dmitry-ra <dmitry.razumikhin@gmail.com>
2016-10-19 19:15:57 +0300

    Add build.cmd and appveyor integration

---
+++ build.cmd
@@ -0,0 +1,46 @@
+@echo off
+SetLocal EnableExtensions EnableDelayedExpansion
+cd %~dp0
+echo Current folder: %cd%
+
+set projects=src\Wiry.Base32 tests\UnitTests
+set temp_dirs=bin obj
+set version_suffix=""
+
+:parse_args
+if not "%1"=="" (
+    if "%1"=="--version-suffix" (
+        set version_suffix="%2"
+        shift
+    )
+    shift
+    goto parse_args
+)
+
+if exist "artifacts" (
+    echo Cleanup artifacts...
+    rmdir /S /Q "artifacts"
+)
+
+for %%p in (%projects%) do (
+    for %%t in (%temp_dirs%) do (
+        set dir=%%p\%%t
+        if exist "!dir!" (
+            echo Removing folder "!dir!"...
+            rmdir /S /Q "!dir!"
+        )
+    )
+)
+
+dotnet restore
+
+dotnet build --configuration Release "src\Wiry.Base32"
+
+dotnet test --configuration Release "tests\UnitTests"
+
+if "!version_suffix!"=="" (
+    dotnet pack "src\Wiry.Base32" --configuration Release --no-build --output "artifacts"
+) else (
+    echo Packing with version suffix !version_suffix!...
+    dotnet pack "src\Wiry.Base32" --configuration Release --no-build --output "artifacts" --version-suffix !version_suffix!
+)
