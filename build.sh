63885bb5bd22b3c5f3a482159f33c9f0e7a9c4ff
dmitry-ra <dmitry.razumikhin@gmail.com>
2016-10-20 01:42:30 +0300

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+exit_handler() {
+    if [[ "$1" != "0" ]]
+    then
+        echo "Error: $1 (command: '$2')"
+        exit 1
+    fi
+    echo "Build succeeded"
+}
+trap 'exit_handler $? "$BASH_COMMAND"' EXIT
+
+sys_name=`uname`
+echo System: $sys_name
+
+dotnet restore
+
+dotnet build --configuration Release --framework netstandard1.6 src/Wiry.Base32
+
+dotnet test --configuration Release tests/UnitTests
