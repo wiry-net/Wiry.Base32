f018a126b4eaac428878b80caddf212df9bc900b
Dmitry Razumikhin <dmitry.razumikhin@gmail.com>
2019-12-28 07:14:38 +0300

    Extract ZBase32Encoding.PadSymbol as a readonly property.

--- src/Wiry.Base32/ZBase32Encoding.cs
+++ src/Wiry.Base32/ZBase32Encoding.cs
@@ -7,20 +7,21 @@ namespace Wiry.Base32
     internal sealed class ZBase32Encoding : Base32Encoding
     {
         protected override string Alphabet => "ybndrfg8ejkmcpqxot1uwisza345h769";
+        private char? PadSymbol => null;
 
         public override string GetString(byte[] bytes, int index, int count)
         {
-            return ToBase32(bytes, index, count, Alphabet, null);
+            return ToBase32(bytes, index, count, Alphabet, PadSymbol);
         }
 
         public override byte[] ToBytes(string encoded, int index, int length)
         {
-            return ToBytes(encoded, index, length, null, GetOrCreateLookupTable(Alphabet));
+            return ToBytes(encoded, index, length, PadSymbol, GetOrCreateLookupTable(Alphabet));
         }
 
         public override ValidationResult Validate(string encoded, int index, int length)
         {
-            return Validate(encoded, index, length, null, GetOrCreateLookupTable(Alphabet));
+            return Validate(encoded, index, length, PadSymbol, GetOrCreateLookupTable(Alphabet));
         }
     }
 }
