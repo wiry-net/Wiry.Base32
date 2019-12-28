facd2cc3efadf87893a25698900bd75482982d72
Dmitry Razumikhin <dmitry.razumikhin@gmail.com>
2019-12-28 07:02:56 +0300

    Change ZBase32Encoding.Alphabet constant to a readonly property.

--- src/Wiry.Base32/ZBase32Encoding.cs
+++ src/Wiry.Base32/ZBase32Encoding.cs
@@ -6,7 +6,7 @@ namespace Wiry.Base32
 {
     internal sealed class ZBase32Encoding : Base32Encoding
     {
-        private const string Alphabet = "ybndrfg8ejkmcpqxot1uwisza345h769";
+        private string Alphabet => "ybndrfg8ejkmcpqxot1uwisza345h769";
 
         public override string GetString(byte[] bytes, int index, int count)
         {
