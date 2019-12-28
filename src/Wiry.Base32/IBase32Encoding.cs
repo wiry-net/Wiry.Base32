9e5dc9ee3bb60176c9e2f40593434cf5a9496673
Dmitry Razumikhin <dmitry.razumikhin@gmail.com>
2019-12-28 06:32:26 +0300

    Add IBase32Encoding interface

---
+++ src/Wiry.Base32/IBase32Encoding.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Dmitry Razumikhin, 2016-2019.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Wiry.Base32
+{
+    /// <summary>
+    /// Base32 encoding interface.
+    /// </summary>
+    public interface IBase32Encoding
+    {
+        /// <summary>
+        /// Encodes bytes to a string.
+        /// </summary>
+        string GetString(byte[] bytes, int index, int count);
+
+        /// <summary>
+        /// Decodes string data to bytes.
+        /// </summary>
+        byte[] ToBytes(string encoded, int index, int length);
+
+        /// <summary>
+        /// Validate input data.
+        /// </summary>
+        ValidationResult Validate(string encoded, int index, int length);
+    }
+}
