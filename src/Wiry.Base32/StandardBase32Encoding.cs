2c78f7d21cdac0f07309e419c4679b88ae3707b7
Dmitry Razumikhin <dmitry.razumikhin@gmail.com>
2019-12-28 07:01:45 +0300

    Change StandardBase32Encoding.Alphabet constant to a readonly property.

--- src/Wiry.Base32/StandardBase32Encoding.cs
+++ src/Wiry.Base32/StandardBase32Encoding.cs
@@ -6,7 +6,7 @@ namespace Wiry.Base32
 {
     internal sealed class StandardBase32Encoding : Base32Encoding
     {
-        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        private string Alphabet => "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 
         public override string GetString(byte[] bytes, int index, int count)
         {
